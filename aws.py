ce1b49276ea8900eb2763b0376a85f674831396d
Pete Ezzo <peter.ezzo@gmail.com>
2016-05-21 12:58:54 -0400

    install some packages on Centos

--- aws.py
+++ aws.py
@@ -2,19 +2,18 @@
 """This is a script that controls a specific AWS instance's lifecycle
    It currently accepts no arguments.  Run once to start devbox.  Run again to terminate."""
 
-
-# import boto3
+import boto3
 
 def ec2_start(resource):
     """Start an AWS EC2 instance
        resource = already open ec2 boto3.resource"""
 
-    # This userdata is specific for a Centos devbox using standalone puppet
+    # TODO: This userdata is specific for a Centos devbox using standalone puppet
     userdata = ('#!/bin/sh\n'
                 'mkdir -p /etc/facter/facts.d\n'
                 'echo "hostgroup=aws" > /etc/facter/facts.d/hostgroup.txt\n'  #used by puppet
-                'rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm\n'  # TODO: centos7 specific
-                'yes | yum -y install git puppet\n' # TODO: centos7 specific
+                'rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm\n'
+                'yes | yum -y install git puppet\n'
                 'mv /etc/puppet /etc/puppet.repo\n'
                 'git clone https://github.com/peterezzo/petenet-puppet.git /etc/puppet\n'
                 'cp /etc/puppet.repo/{auth.conf,puppet.conf} /etc/puppet\n'
@@ -32,7 +31,7 @@ def ec2_start(resource):
         UserData=userdata,
         BlockDeviceMappings=[
             {
-                'DeviceName': '/dev/sda1',  #/dev/xvdh?
+                'DeviceName': '/dev/sda1',  # root for ami, sometimes /dev/xvdh ?
                 'Ebs': {
                     'VolumeSize': 20,
                     'DeleteOnTermination': True,
@@ -63,7 +62,6 @@ def ec2_stop(resource, instance_id):
 def main():
     """This is the main body of the program
        Receives no arguments"""
-    import boto3
 
     ec2 = boto3.resource('ec2')
 
@@ -80,7 +78,7 @@ def main():
             # public IP is only allocated when system is running
             instance.wait_until_running()
             instance.load()
-            print("id", instance.id,"\naddress", instance.public_ip_address)
+            print("id", instance.id, "\naddress", instance.public_ip_address)
     else:
         print(count, "instances running, killing VM(s)")
         for instance in instances:
